d23246274558e65d3dcedc2936c8c914b4097c6c
Rajiv Giri <rajivgiri513@gmail.com>
2024-04-10 11:30:48 -0400

    Initial Commit

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+
+function NotFound() {
+
+    return <div>
+
+        <h1>404 NOT FOUND</h1>
+        <p>The page you are looking for doesn't exists!</p>
+        
+        </div>
+}
+
+export default NotFound
